2439be412c8269c3aac6eefd5190cae632d0d309
Hikmat <94689644+HikmatKhiva@users.noreply.github.com>
2024-02-09 12:08:11 +0500

    update

--- src/components/AboutCard.jsx
+++ src/components/AboutCard.jsx
@@ -1,18 +1,23 @@
 import PropTypes from "prop-types";
 import { motion } from "framer-motion";
 import { fadeAnimation } from "../motion/variants";
+import test from "../assets/svg/progress.svg";
 const AboutCard = ({ about, index }) => {
   return (
     <motion.figure
       variants={fadeAnimation("right", 4, 0.5 * index)}
       initial="start"
       animate="finished"
-      className="w-[calc(50%_-_40px)] "
+      className="w-[calc(50%_-_40px)] relative"
     >
-      <div className="w-9 h-9 rounded-lg about-card flex justify-center items-center mb-2">
-        <img src={about.icon} alt={about.title} />
+      <div className=" rounded-lg  flex  items-center mb-2">
+        <img
+          src={about.icon}
+          alt={about.title}
+          className="absolute -left-7 object-cover"
+        />
       </div>
-      <figcaption>
+      <figcaption className="mt-7">
         <h3 className="text-white font-intern text-xl font-medium mb-2">
           {about.title}
         </h3>
